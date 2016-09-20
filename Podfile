e86e63375c04b8f5da8d240960a61a9187142cf1
ShayneYeorg <mercury10@qq.com>
2016-09-20 16:28:44 +0800

    玩一下Socket

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'TestSocketConnect' do
+  # Uncomment this line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+
+platform :ios, '9.0'
+pod 'CocoaAsyncSocket'
+
+  # Pods for TestSocketConnect
+
+end
---
+++ TestSocketConnect/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestSocketConnect
+//
+//  Created by 杨淳引 on 16/9/20.
+//  Copyright © 2016年 shayneyeorg. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
