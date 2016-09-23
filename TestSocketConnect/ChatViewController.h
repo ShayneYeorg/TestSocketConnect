8e5346a4b1194e4970d7349738a61a02fd86359f
ShayneYeorg <mercury10@qq.com>
2016-09-23 15:47:16 +0800

    主页上线下线控制

---
+++ TestSocketConnect/ChatViewController.h
@@ -0,0 +1,14 @@
+//
+//  ChatViewController.h
+//  TestSocketConnect
+//
+//  Created by 杨淳引 on 16/9/23.
+//  Copyright © 2016年 shayneyeorg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ShYSocketManager.h"
+
+@interface ChatViewController : UIViewController
+
+@end
--- TestSocketConnect/ViewController.m
+++ TestSocketConnect/ViewController.m
@@ -7,10 +7,10 @@
 //
 
 #import "ViewController.h"
-#import "ShYSocketManager.h"
+#import "ChatViewController.h"
 
-//static NSString *MODULE_NAME = @"ViewController";
-//static const long TAG = 0;
+static NSString *MODULE_NAME = @"MainVC";
+static const long TAG = 0;
 
 @interface ViewController () <UITableViewDelegate,UITableViewDataSource>
 //@property (weak, nonatomic) IBOutlet UITableView *tblMessageContent;
@@ -26,13 +26,26 @@
 //@property (strong, nonatomic) NSMutableArray *arrMessages;
 //@property (strong, nonatomic) NSString *nickName;
 
+@property (weak, nonatomic) IBOutlet UITextField *ip;
+@property (weak, nonatomic) IBOutlet UITextField *port;
+@property (weak, nonatomic) IBOutlet UITextField *user;
+@property (weak, nonatomic) IBOutlet UIButton *onOffLineBtn;
+
+@property (nonatomic, strong) ChatViewController *chatVC0; //聊天室0
+@property (nonatomic, strong) ChatViewController *chatVC1; //聊天室1
+
 @end
 
 @implementation ViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.title = @"Socket Demo - 未上线";
+    
+    self.chatVC0 = [ChatViewController new];
+    self.chatVC1 = [ChatViewController new];
     
     
     
@@ -51,41 +64,53 @@
 //    
 //    self.nickName = @"模拟器";
 //    [self setupCallbacks];
-//    
-//    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(receiveSocketDidConnectNotification:) name:SOCKET_DID_CONNECT object:nil];
-//    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(receiveSocketDidDisconnectNotification:) name:SOCKET_DID_DISCONNECT object:nil];
+//
+    
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(receiveSocketDidConnectNotification:) name:SOCKET_DID_CONNECT object:nil];
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(receiveSocketDidDisconnectNotification:) name:SOCKET_DID_DISCONNECT object:nil];
 }
 
-//- (void)dealloc {
-//    ShYSocketManager *socketManager = [ShYSocketManager share];
-//    [socketManager disconnect];
-//}
+- (void)dealloc {
+    ShYSocketManager *socketManager = [ShYSocketManager share];
+    [socketManager disconnect];
+}
 
-//- (IBAction)connectAction:(id)sender {
-//    [self.view endEditing:true];
-//    ShYSocketManager *socketManager = [ShYSocketManager share];
-//    NSString * strAddr = [_txtAddress text];
-//    NSString * strPort = [_txtPort text];
-//    if (strAddr.length && strPort.length) {
-//        NSError * error = nil;
-//        //连接服务器
-//        [socketManager connectToHost:strAddr onPort:strPort error:&error];
-//        if (error) {
-//            NSLog(@"连接失败: %@", error.description);
-//            
-//        } else {
-//            //告诉全世界,哥上线了
-//            NSDictionary *dic = @{@"user":self.nickName, @"module":MODULE_NAME, OPERATION: OPERATION_ONLINE};
-//            [socketManager sendMessage:dic tag:TAG];
-//            [self.arrMessages addObject:dic];
-//            [_tblMessageContent reloadData];
-//            [_tblMessageContent scrollToRowAtIndexPath:[NSIndexPath indexPathForRow:self.arrMessages.count-1 inSection:0] atScrollPosition:UITableViewScrollPositionBottom animated:true];
-//        }
-//        
-//    }else{
-//        NSLog(@"请将地址信息填写完整");
-//    }
-//}
+
+- (IBAction)onOffLineBtnClick:(id)sender {
+    [self.view endEditing:true];
+    ShYSocketManager *socketManager = [ShYSocketManager share];
+    
+    if (![socketManager isConnected]) {
+        //要上线
+        if (self.ip.text.length && self.port.text.length && self.user.text.length) {
+            NSError * error = nil;
+            //连接服务器
+            [socketManager connectToHost:self.ip.text onPort:self.port.text error:&error];
+            if (error) {
+                NSLog(@"连接失败: %@", error.description);
+                
+            } else {
+                //告诉全世界,哥上线了
+                NSDictionary *dic = @{@"user":self.user.text, @"module":MODULE_NAME, OPERATION: OPERATION_ONLINE};
+                [socketManager sendMessage:dic tag:TAG];
+            }
+            
+        }else{
+            NSLog(@"请将相关信息填写完整");
+        }
+        
+        
+    } else {
+        //要下线
+        //告诉全世界，哥下线了
+        NSDictionary *dic = @{@"user":self.user.text, @"module":MODULE_NAME, OPERATION: OPERATION_OFFLINE};
+        [socketManager sendMessage:dic tag:TAG];
+        [socketManager disconnect];
+    }
+}
+
+
+
 //
 //- (IBAction)sendAction:(id)sender {
 //    [self.view endEditing:true];
@@ -106,19 +131,6 @@
 //    }
 //}
 //
-//- (IBAction)btnDisconnect:(id)sender {
-//    ShYSocketManager *socketManager = [ShYSocketManager share];
-//    if ([socketManager isConnected]) {
-//        //告诉全世界，哥下线了
-//        NSDictionary *dic = @{@"user":self.nickName, @"module":MODULE_NAME, OPERATION: OPERATION_OFFLINE};
-//        [socketManager sendMessage:dic tag:TAG];
-//        [self.arrMessages addObject:dic];
-//        [_tblMessageContent reloadData];
-//        [_tblMessageContent scrollToRowAtIndexPath:[NSIndexPath indexPathForRow:self.arrMessages.count-1 inSection:0] atScrollPosition:UITableViewScrollPositionBottom animated:true];
-//        
-//        [socketManager disconnect];
-//    }
-//}
 //
 //- (void)setupCallbacks {
 //    ShYSocketManager *socketManager = [ShYSocketManager share];
@@ -134,21 +146,26 @@
 //        [weakSelf.tblMessageContent scrollToRowAtIndexPath:[NSIndexPath indexPathForRow:self.arrMessages.count-1 inSection:0] atScrollPosition:UITableViewScrollPositionBottom animated:true];
 //    } module:MODULE_NAME];
 //}
-//
-//- (void)receiveSocketDidConnectNotification:(NSNotification *)notification {
-//    [_btnConnect setEnabled:false];
-//    [_btnDisconnect setEnabled:true];
-//}
-//
-//- (void)receiveSocketDidDisconnectNotification:(NSNotification *)notification {
-//    [_btnConnect setEnabled:true];
-//    [_btnDisconnect setEnabled:false];
-//}
+
+
+
+- (void)receiveSocketDidConnectNotification:(NSNotification *)notification {
+    [self.onOffLineBtn setTitle:@"下线" forState:UIControlStateNormal];
+    self.onOffLineBtn.backgroundColor = [UIColor redColor];
+    self.title = @"Socket Demo - 已上线";
+}
+
+- (void)receiveSocketDidDisconnectNotification:(NSNotification *)notification {
+    [self.onOffLineBtn setTitle:@"上线" forState:UIControlStateNormal];
+    self.onOffLineBtn.backgroundColor = [UIColor brownColor];
+    self.title = @"Socket Demo - 未上线";
+}
 
 #pragma mark - UITableViewDelegate
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:NO];
+    [self.navigationController pushViewController:[ChatViewController new] animated:YES];
 }
 
 #pragma mark - UITableViewDataSource
